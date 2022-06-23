ee427213cedd7c7dcc5eb8d5f025dcd452a5012b
cklamann <12862284+cklamann@users.noreply.github.com>
2022-06-23 14:09:03 -0400

    use dev image to build schema

--- build-schema.sh
+++ build-schema.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-docker run --entrypoint="" -v --rm ghcr.io/perceptimatic/sfo-shennong-runner:latest python3 /code/app/parse_shennong.py > ./shennong_runner/processor-schema.json
+docker run --entrypoint="" -v --rm ghcr.io/perceptimatic/sfo-shennong-runner:dev python3 /code/app/parse_shennong.py > ./shennong_runner/processor-schema.json
 
 cp ./shennong_runner/processor-schema.json ./api/static/ 
 cp ./shennong_runner/processor-schema.json ./worker/
